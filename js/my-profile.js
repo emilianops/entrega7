5f1a7404fd56e2ddd8ea17052914fcd63f157b9e
Rodrigo Gorga <108111681+gorga7@users.noreply.github.com>
2023-10-28 13:15:39 -0300

    Update my-profile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,5 @@
+
+ //INICIO ENTREGA 7 PARTE 1
 function isLoggedIn() {
     let dato = localStorage.getItem("email");
     if (dato){
@@ -7,13 +9,13 @@ function isLoggedIn() {
     }
 }
 
-// entrea 4, parte 2: función para cerrar sesión
+//función para cerrar sesión
 function cerrarSesion() {
     localStorage.removeItem("email");
     window.location.href = 'login.html'; //redirige al inicio
 }
 
-// entrega 4, parte 2: evento click cerrar sesión
+//evento click cerrar sesión
 document.getElementById("cerrarSesion").addEventListener("click", function() {
     cerrarSesion();
 } );
@@ -25,7 +27,40 @@ window.addEventListener('load', function() {
     }
 });
 
+//FIN ENTREGA 7 PARTE 1
 
+
+
+
+//INICIO ENTREGA 7 PARTE 2
+
+
+//FIN ENTREGA 7 PARTE 2
+
+
+
+
+
+
+//INICIO ENTREGA 7 PARTE 3
+
+
+//FIN ENTREGA 7 PARTE 3
+
+
+
+
+
+//INICIO ENTREGA 7 DESAFIATE
+
+
+//FIN ENTREGA 7 DESAFIATE
+
+
+
+
+
+//OTRAS FUNCIONES NECESARIAS
 
 const btnTema = document.getElementById('btnTema');
 const body = document.body;
@@ -55,12 +90,10 @@ btnTema.addEventListener('click', toggleTheme);
 
 
 
+
+//Muestra en la página el email del usuario
 let email = localStorage.getItem("email"); // <- email = "emilianopintos18@gmail.com"
 
-
-
-
 let li_nav = document.getElementById("usuario");
-
 
 li_nav.innerHTML = `<span class="nav-link">${email}</span>`;
